d8ad1ab3c5108769306c4e4ff9302a15994681d6
Shehryar Ehtesham <se318@rutgers.edu>
2019-11-09 22:56:46 -0500

    wrote out instructions for js file

---
+++ cheatsheet.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const table = require("table");
+const chalk = require("chalk");
+
+//Create file system with fs 
+//install table and chalk modules for styling (later)
+
+//Create a route-switch case statement for CRUD operations
+// Create a file
+// Read a file
+// Updated a file
+// Delete a file
+// Show list of Sheets
+
+
